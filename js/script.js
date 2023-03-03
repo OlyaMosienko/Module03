f5f41b202ba9402643e3048188b903cacce7acb8
olya <olya.ololoeva81@mail.ru>
2023-03-03 22:28:15 +0300

    add files

---
+++ js/script.js
@@ -0,0 +1,47 @@
+'use strict';
+// let result = prompt('Text');
+// console.log(2*3);
+// let i = 2;
+// i = -i;
+// console.log(i);
+// let x = 2, y = 3;
+// console.log(y-x);
+// console.log(5%2);
+// console.log(2**3);
+// console.log(4**(1/2));
+
+// let x = true;
+// console.log(+x);
+
+// let x = "1";
+// let y = "2";
+// console.log(+x + +y);
+
+//let x = 2+2*2;
+//console.log(x);
+
+// let n=2;
+// n+=5; // n=n+5
+// console.log(n); 
+// n*=2; // n=n*2
+// console.log(n);
+
+// let a=3;
+// a*=5+3;
+// console.log(a);
+
+// let a = (1+2,3+4);
+// console.log(a);
+
+// console.log(true||true); // true
+// console.log(false||true); // true
+// console.log(true||false); // true
+// console.log(false||false); // false
+
+// console.log(true&&true); // true
+// console.log(false&&true); // false
+// console.log(true&&false); // false
+// console.log(false&&false); // false
+
+
+
